1bc6d011ee478b175313314788d901072df039ff
cwts007 <116644442+cwts007@users.noreply.github.com>
2024-06-23 18:19:58 -0400

    listo para deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer style={{ textAlign: 'center', padding: '10px', background: '#eeeeee', marginTop: '20px' }}>
+        <footer className="footer">
             <p>&copy; 2024 Pizzería Mamma Mia! Todos los derechos reservados.</p>
+            <p>
+                Desarrollado por Christopher W. Thompson Stone. para desafio final de modulo react 2 en Deafio Latam
+                <br />
+                <a href="https://github.com/cwts007/" target="_blank" rel="noopener noreferrer">
+                    Mi GitHub
+                </a>
+            </p>
         </footer>
     );
 };
